310536536492d87d6aeca2f7d5e32adde9361fcd
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 03:18:38 -0400

    baselines: export labeled examples

--- src/intentfidelity/baselines/__init__.py
+++ src/intentfidelity/baselines/__init__.py
@@ -2,11 +2,21 @@
 
 from intentfidelity.baselines.registry import BaselineSpec, get_baseline, list_baselines
 from intentfidelity.baselines.predictions import proxy_oracle_prediction, uniform_prediction
+from intentfidelity.baselines.examples import (
+    LabeledExample,
+    feature_dimension,
+    labels_from_examples,
+    sessions_from_examples,
+)
 
 __all__ = [
     "BaselineSpec",
+    "LabeledExample",
+    "feature_dimension",
     "get_baseline",
+    "labels_from_examples",
     "list_baselines",
     "proxy_oracle_prediction",
+    "sessions_from_examples",
     "uniform_prediction",
 ]
