e5c1e8ddc0fa1fb8378e0ac9f02346192ca9570b
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 02:50:06 -0400

    labels: export cue text utilities

--- src/intentfidelity/labels/__init__.py
+++ src/intentfidelity/labels/__init__.py
@@ -15,15 +15,25 @@ from intentfidelity.labels.handwriting import (
     HandwritingCue,
     weak_target_from_handwriting_cue,
 )
+from intentfidelity.labels.cue_text import (
+    SPACE_TOKEN,
+    cue_character_sequence,
+    cue_support,
+    normalize_h2_cue,
+)
 
 __all__ = [
     "DistributionValidationError",
     "HANDWRITING_SOURCE_TYPE",
     "Prediction",
+    "SPACE_TOKEN",
     "HandwritingCue",
     "WeakTarget",
     "align_probabilities",
+    "cue_character_sequence",
+    "cue_support",
     "normalize_probabilities",
+    "normalize_h2_cue",
     "prediction_from_dict",
     "require_same_support",
     "weak_target_from_dict",
