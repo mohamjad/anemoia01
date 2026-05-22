e2fdfc02f96234b36ba0ff667d598acce031e710
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 02:52:58 -0400

    baselines: export prediction baselines

--- src/intentfidelity/baselines/__init__.py
+++ src/intentfidelity/baselines/__init__.py
@@ -1,6 +1,12 @@
 """Baseline method registry."""
 
 from intentfidelity.baselines.registry import BaselineSpec, get_baseline, list_baselines
+from intentfidelity.baselines.predictions import proxy_oracle_prediction, uniform_prediction
 
-__all__ = ["BaselineSpec", "get_baseline", "list_baselines"]
-
+__all__ = [
+    "BaselineSpec",
+    "get_baseline",
+    "list_baselines",
+    "proxy_oracle_prediction",
+    "uniform_prediction",
+]
