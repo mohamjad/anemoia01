799f122edcf413ad61c1bf8297b09ece3218b685
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 03:22:33 -0400

    baselines: export feature transforms

--- src/intentfidelity/baselines/__init__.py
+++ src/intentfidelity/baselines/__init__.py
@@ -8,15 +8,27 @@ from intentfidelity.baselines.examples import (
     labels_from_examples,
     sessions_from_examples,
 )
+from intentfidelity.baselines.transforms import (
+    FeatureTransform,
+    identity_transform,
+    session_centering_transform,
+    whitening_coloring_transform,
+    whitening_transform,
+)
 
 __all__ = [
     "BaselineSpec",
+    "FeatureTransform",
     "LabeledExample",
     "feature_dimension",
     "get_baseline",
+    "identity_transform",
     "labels_from_examples",
     "list_baselines",
     "proxy_oracle_prediction",
     "sessions_from_examples",
+    "session_centering_transform",
     "uniform_prediction",
+    "whitening_coloring_transform",
+    "whitening_transform",
 ]
