d6dbb6a481b1dce225407599dc655b510f75b021
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 02:44:49 -0400

    ingest: export hdf5 helpers

--- src/intentfidelity/ingest/__init__.py
+++ src/intentfidelity/ingest/__init__.py
@@ -13,6 +13,14 @@ from intentfidelity.ingest.falcon_h2 import (
     inventory_falcon_h2,
     resolve_falcon_h2_root,
 )
+from intentfidelity.ingest.nwb_hdf5 import (
+    Hdf5DatasetInfo,
+    NwbParseError,
+    decode_array,
+    decode_scalar,
+    list_hdf5_datasets,
+    require_dataset,
+)
 
 __all__ = [
     "DataFile",
@@ -22,6 +30,12 @@ __all__ = [
     "FALCON_H2_EXPECTED_SPLITS",
     "ValidationIssue",
     "ValidationSeverity",
+    "Hdf5DatasetInfo",
+    "NwbParseError",
+    "decode_array",
+    "decode_scalar",
     "inventory_falcon_h2",
+    "list_hdf5_datasets",
+    "require_dataset",
     "resolve_falcon_h2_root",
 ]
