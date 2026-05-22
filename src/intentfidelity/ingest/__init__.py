1338da02565bc539ef3a827c2b10fda87607fad3
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 02:47:29 -0400

    ingest: export falcon trial loader

--- src/intentfidelity/ingest/__init__.py
+++ src/intentfidelity/ingest/__init__.py
@@ -22,6 +22,7 @@ from intentfidelity.ingest.nwb_hdf5 import (
     require_dataset,
 )
 from intentfidelity.ingest.falcon_session import FalconSessionKey, session_date_from_path
+from intentfidelity.ingest.falcon_trials import FalconH2Trial, load_falcon_h2_trials
 
 __all__ = [
     "DataFile",
@@ -30,6 +31,7 @@ __all__ = [
     "FALCON_H2_DATASET_ID",
     "FALCON_H2_EXPECTED_SPLITS",
     "FalconSessionKey",
+    "FalconH2Trial",
     "ValidationIssue",
     "ValidationSeverity",
     "Hdf5DatasetInfo",
@@ -38,6 +40,7 @@ __all__ = [
     "decode_scalar",
     "inventory_falcon_h2",
     "list_hdf5_datasets",
+    "load_falcon_h2_trials",
     "require_dataset",
     "resolve_falcon_h2_root",
     "session_date_from_path",
