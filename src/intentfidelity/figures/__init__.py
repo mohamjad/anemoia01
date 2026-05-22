3efc1acdde26a302807f512a44edb308b5c8b088
mohamjad <mohammedamjad526@gmail.com>
2026-05-22 03:50:34 -0400

    figures: export comparison table renderer

--- src/intentfidelity/figures/__init__.py
+++ src/intentfidelity/figures/__init__.py
@@ -1,6 +1,6 @@
 """Figure-generation entry points."""
 
 from intentfidelity.figures.ranking import render_ranking_reversal
+from intentfidelity.figures.comparison import render_comparison_table
 
-__all__ = ["render_ranking_reversal"]
-
+__all__ = ["render_comparison_table", "render_ranking_reversal"]
